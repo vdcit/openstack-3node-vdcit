219f41b728667e9090e82e7b2b3cb826e0a9cb78
congto <tcvn1985@gmail.com>
2014-08-28 14:43:52 +0700

    Update code cho Ubuntu14
    Update code cho Ubuntu14

---
+++ script-U1404-VLAN/control-1.ipadd.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -ex
+source config.cfg
+
+ifaces=/etc/network/interfaces
+test -f $ifaces.orig || cp $ifaces $ifaces.orig
+rm $ifaces
+touch $ifaces
+cat << EOF >> $ifaces
+#Dat IP cho $HOST_NAME node
+
+# LOOPBACK NET 
+auto lo
+iface lo inet loopback
+
+# MGNT NETWORK
+auto eth0
+iface eth0 inet static
+address $CON_MGNT_IP
+netmask $NETMASK_ADD_VM
+
+
+# VLANs NETWORK
+auto eth1
+iface eth1 inet manual
+up ifconfig \$IFACE 0.0.0.0 up
+up ip link set \$IFACE promisc on
+down ifconfig \$IFACE 0.0.0.0 down
+EOF
+
+echo "Cau hinh hostname cho $HOST_NAME NODE"
+sleep 3
+echo "$HOST_NAME" > /etc/hostname
+hostname -F /etc/hostname
+
+echo "##### Khoi dong lai may #####"
+sleep 5
+init 6
+#
