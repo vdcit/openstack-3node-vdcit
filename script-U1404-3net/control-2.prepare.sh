d72d1584e748774053f247d1f4ee75239c9d9de3
Tô Thành Công <tcvn1985@gmail.com>
2014-09-03 10:34:11 +0700

    Thay doi repos

--- script-U1404-3net/control-2.prepare.sh
+++ script-U1404-3net/control-2.prepare.sh
@@ -19,13 +19,13 @@ EOF
 
 # Cai dat repos va update
 # Neu la ubuntu 12 chay 2 lenh duoi
-apt-get install -y python-software-properties &&  add-apt-repository cloud-archive:icehouse -y 
-apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
+# apt-get install -y python-software-properties &&  add-apt-repository cloud-archive:icehouse -y 
+# apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
 
 # Neu la ubuntu 14.04 chay 3 lenh duoi
-# apt-get update -y
-# apt-get upgrade -y
-# apt-get dist-upgrade -y
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
 
 echo "Cai dat NTP va cau hinh NTP"
 sleep 3 
